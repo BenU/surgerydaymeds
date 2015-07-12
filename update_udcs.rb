63265e6316add4fadf5ca4ebb6330152c52878fc
Benjamin Unger <benjamin.d.unger@gmail.com>
2015-07-12 09:00:23 -0400

    modify update_udcs.rb to enclose rules with single quotes

--- update_udcs.rb
+++ update_udcs.rb
@@ -19,15 +19,13 @@ drug_class_csv = CSV.parse(drug_class_text, headers: false)
 
 dc_h = {}
 drug_class_csv.each do |row|
-  dc_h[row[0]] = [row[1], row[2]]
+  dc_h[row[0]] = [row[1], row[2], row[3], row[4]]
 end
 
 drug_file_url = "https://raw.githubusercontent.com/BenU/surgerydaymeds/master/unique_drugs.csv"
 drug_text = open(drug_file_url)
 drug_csv = CSV.parse(drug_text, headers: false)
 
-new_dcs = []
-old_dcs = []
 drug_csv.each do |drug|
   epcs = find_epcs(drug[13])
   unless epcs == "No Pharm Classes"
@@ -43,7 +41,10 @@ end
 n = 1
 open('epcs_with_rules.csv', 'w') do |u|
   dc_h.sort.each do |dc,rule| 
-    dcs ="#{dc},#{rule.join(',')}"
+    rule_number = rule[0]
+    rule_string = rule[1]
+    rule_string += ",#{rule[2]}" unless rule[2].nil?
+    dcs ="#{dc},#{rule_number},'#{rule_string}'"
     puts "#{n}: #{dcs}"
     n += 1
     u << dcs << "\n"
